0f0d56647ed781e356639505345722217a32de6c
Joseph Lehr <jlehr@bitiverse.io>
2019-11-10 00:14:28 -0700

    added deploy.ts and stub response for getting the latest sls deploy template json

--- handler.ts
+++ handler.ts
@@ -1,12 +1,17 @@
 import 'source-map-support/register';
+import {Deploy} from './deploy';
+import S3 from 'aws-sdk/clients/s3';
 
 export const initstack = async (event, _context) => {
+  const deploy = new Deploy(new S3());
+  const slsTemplateObject = await deploy.getLatestSLSDeployJson("bitiverse-sls-deployments", "serverless/vfi/dev/");
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Lambda Works!',
-      input: event,
-      env: process.env
+      templateKey: slsTemplateObject.Key
+      // input: event,
+      // env: process.env
     }, null, 2),
   };
 }
