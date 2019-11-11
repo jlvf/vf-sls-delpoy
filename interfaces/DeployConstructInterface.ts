0de20ae7395333c1d8387cee8a6c46a34c2d656e
Joseph Lehr <jlehr@bitiverse.io>
2019-11-10 17:48:21 -0700

    created interfaces for DynamoDb deploy table and for the object that the Deploy.ts class constructor receives

---
+++ interfaces/DeployConstructInterface.ts
@@ -0,0 +1,11 @@
+import S3 from "aws-sdk/clients/s3";
+import { DynamoDB } from "aws-sdk/clients/all";
+
+export interface DeployConstructInterface {
+    s3: S3;
+    ddbClient: DynamoDB.DocumentClient;
+    s3Bucket: string;
+    s3Prefix?: string;
+    slsServiceName: string;
+    tableName: string;
+}
---
+++ interfaces/DeployTableSchemaInterface.ts
@@ -0,0 +1,5 @@
+export interface DeployTableSchemaInterface {
+    service_name: string;
+    created_utc_ms: number;
+    template: object;
+}
