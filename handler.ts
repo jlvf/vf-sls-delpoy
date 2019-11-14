0f6178ada8ce56fa7b38758e138aa3935ac28508
Joseph Lehr <jlehr@bitiverse.io>
2019-11-13 22:45:29 -0700

    moved router to its own file

--- handler.ts
+++ handler.ts
@@ -1,49 +1,8 @@
 import 'source-map-support/register';
-import {Deploy} from './deploy';
-import S3 from 'aws-sdk/clients/s3';
-import { DynamoDB } from 'aws-sdk/clients/all';
-import { ResponseInterface } from './interfaces/ResponseInterface';
 
-export const initstack = async (event, _context) => {
-  const lambdastart = new Date().getTime();
-  if (typeof event === "string") {
-    event = JSON.parse(event);
-  }
-  const response: ResponseInterface = {
-    statusCode: 200,
-    body: {}
-  };
-  if (typeof event.deploytime_utc_ms === "undefined") {
-    if (typeof event.slsd !== "undefined") {
-      response.statusCode = 500;
-      response.body.message = "deploytime_utc_ms is required";
-      return response;
-    }
-  }
-  const deploy = new Deploy({
-    s3: new S3(), 
-    ddbClient: new DynamoDB.DocumentClient(),
-    s3Bucket: process.env.SLS_DEPLOY_BUCKET,
-    s3Prefix: process.env.SLS_BUCKET_PREFIX,
-    tableName: process.env.DDB_TABLE_NAME,
-    slsServiceName: process.env.SLS_SERVICE_NAME,
-    deploytime_utc_ms: event.deploytime_utc_ms
-  });
-  if (typeof event.slsd !== "undefined") {
-    const slsd = await deploy.insertLatestSLSTemplate();
-    if (slsd) {
-      response.body.message = process.env.SLS_SERVICE_NAME + " has been deployed";
-    } else {
-      response.body.message = process.env.SLS_SERVICE_NAME + " template could not be inserted into " + process.env.DDB_TABLE_NAME
-    }
-  }
-  if (typeof event.stats !== "undefined") {
-    response.body.stats = await deploy.getDeploymentStats();
-  }
-  if (typeof event.view !== "undefined") {
-    response.body.template = JSON.stringify(deploy.template);
-  }
+import { Router } from './Router';
 
-  response.body.lambdaRuntime = deploy.convertMsToTime((new Date().getTime()) - lambdastart);
-  return response;
+export const initstack = async (event, context) => {
+  const router = new Router(event, context);
+  return await router.response();
 }
